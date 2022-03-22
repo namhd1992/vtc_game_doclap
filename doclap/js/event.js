9e139a79518f0a4ae1a4adc319efad23d836d8e7
namhd1992 <stpnghd.vn@gmail.com>
2022-03-22 17:02:53 +0700

    fix

---
+++ doclap/js/event.js
@@ -0,0 +1,60 @@
+
+function goToFanpage(){
+
+}
+
+function goToFanpage(){
+
+}
+
+function getGoal(value){
+    alert(value)
+}
+
+function playGame(type){
+
+}
+
+function getHistory(page){
+
+}
+
+function getGiftcodeByNumberPlay(value){
+
+}
+
+function getKeyGoal(){
+
+}
+
+function getGiftcodeByNumberBocBanh(value){
+
+}
+
+function playFlipCard(){
+
+}
+
+function getHistoryplayFlipCard(){
+
+}
+
+function rewardExchangeMai(value){
+
+}
+
+function rewardExchangeDao(value){
+
+}
+
+function openRuong(){
+
+}
+
+function getBXHLiXi(){
+
+}
+
+function getRewardTop(value){
+
+}
