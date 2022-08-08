c1b768c4be35e92cca9b83d96ae7f46ae75fb4fb
namhd1992 <stpnghd.vn@gmail.com>
2022-08-08 15:54:05 +0700

    fix

---
+++ Ramdom_SDT/data.js
@@ -0,0 +1,10 @@
+const list_phone=[
+    100,200,300,400,500,600,700,800,900
+]
+
+const list_number=[
+    0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16
+]
+
+const win_phone='035xxxxxxxx';
+const win_number=''
--- Ramdom_SDT/index.js
+++ Ramdom_SDT/index.js
@@ -1,21 +1,60 @@
 const game_client={
-    var result='',
-    var isPlay='',
-    var time=3,
+    result:'',
+    isPlay:false,
+    time:3,
+    case_1:false,
+    case_2:false,
+    data_live:[],
+    
+
     initApp(){
-        var a =document.getElementById('txtareadayso')
-        console.log(a)
+        game_client.setResult();
+        game_client.data_live=list_phone;
+        $("#phonenumber").prop("checked", true);
+    },
+
+    random_one(){
+        $("#phonenumber").prop("checked", true);
+        $("#one_to_hundred").prop("checked", false);
+        game_client.data_live=list_phone;
+    },
+
+    random_two(){
+        $("#phonenumber").prop("checked", false);
+        $("#one_to_hundred").prop("checked", true);
+        game_client.data_live=list_number;
     },
 
     changeSpeed(){
         var e = document.getElementById("slTocDo2");
-        time = e.value;
-        console.log(time)
+        game_client.time = e.value;
     },
 
     getArrayInfo(){
         var e = document.getElementById("txtareadayso");
-        time = e.value;
+        var list_phone=e.value.split(',')
+        game_client.lucky(e.value.split(','))
+    },
+
+    lucky(list){
+        if(!game_client.isPlay){
+            game_client.isPlay=true;
+            var inter=setInterval(()=>{
+                const random = Math.floor(Math.random() * list.length);
+                game_client.result=list[random];
+                game_client.setResult();
+            },100)
+            setTimeout(() => {
+                clearInterval(inter)
+                game_client.isPlay=false;
+                game_client.result=list[random];
+            }, game_client.time*1000);
+        }
+    },
+
+    setResult(){
+        var a =document.getElementById('loadingnha')
+        a.innerHTML=this.result;
     },
 
     randomPhone_Test(){
@@ -23,7 +62,7 @@ const game_client={
     },
 
     randomPhone_Live(){
-
+        game_client.lucky(game_client.data_live)
     }
 
 }
